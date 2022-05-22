a28c4b33cacd49c8ee82d85bef94820246c7fc82
Daniela Klem de Castro Pinheiro <danie_klem@hotmail.com>
2022-05-22 19:30:41 -0300

    Modificações em desafio.js e index.html

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,12 +11,34 @@
  * 
  */
 
+// Funções
+function novaCarta(cartas) {
+   let novaCarta = comprarCarta()
+   cartas.push(novaCarta)
+}
+function calculaPontuacao(cartas) {
+   let soma = 0
+   for(let i = 0; i < cartas.length; i++) {
+      soma += cartas[i].valor
+   }
+   return soma
+}
+function retornaCartasReveladas(cartas) {
+   let cartasReveladas = []
+   for(let i = 0; i < cartas.length; i++) {
+      cartasReveladas.push(cartas[i].texto)
+   }
+   return cartasReveladas.join(" ")
+}
 
+// Variaveis
+let pontuacaoUsuario = 0
+let pontuacaoComputador = 0
 
 // Iniciando uma nova rodada
 if(confirm("Boas vindas ao jogo de BlackJack!" +
-"\n" +
-"Quer iniciar uma nova rodada?")) {
+   "\n" +
+   "Quer iniciar uma nova rodada?")) {
    // Sorteia duas cartas para cada jogador
    let cartasUsuario = [comprarCarta(), comprarCarta()]
    let cartasComputador = [comprarCarta(), comprarCarta()]
@@ -27,10 +49,92 @@ if(confirm("Boas vindas ao jogo de BlackJack!" +
       cartasComputador = [comprarCarta(), comprarCarta()]
    }
 
-   // 9.
-   // 10.
-   // 11.
-   // 12.
+   // Vez do Usuário:
+   // Pode escolher comprar mais cartas
+   if(confirm(
+   `Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. A carta revelada do computador é ${cartasComputador[0].texto}.` +
+   "\n" +  
+   "Deseja comprar mais uma carta?")) {
+      // Compra mais uma carta
+      novaCarta(cartasUsuario)
+      // Faz a pontuação
+      pontuacaoUsuario = calculaPontuacao(cartasUsuario)
+
+      // Se a pontuação for menor de 21 pode comprar novamente
+      if(pontuacaoUsuario < 21) {
+         if(confirm(
+         `Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. A carta revelada do computador é ${cartasComputador[0].texto}.` +
+         "\n" +  
+         "Deseja comprar mais uma carta?")) {
+            // Compra mais uma carta
+            novaCarta(cartasUsuario)
+            // Faz a pontuação
+            pontuacaoUsuario = calculaPontuacao(cartasUsuario)
+         } else {
+            // Faz a pontuação
+            pontuacaoUsuario = calculaPontuacao(cartasUsuario)
+            // Fim da vez do Usuário
+         }
+      } else if(pontuacaoUsuario > 21) {
+         // Faz a pontuação
+         pontuacaoUsuario = calculaPontuacao(cartasUsuario)
+         pontuacaoComputador = calculaPontuacao(cartasComputador)
+         // Mensagem de derrota: quando a pontuação for maior de 21
+         alert(
+         `Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. Sua pontuação é ${pontuacaoUsuario}.` +
+         "\n" + 
+         `As cartas do computador são ${retornaCartasReveladas(cartasComputador)}. A pontuação do computador é ${pontuacaoComputador}.` +
+         "\n" +
+         "O computador ganhou!")
+      }      
+   } else {
+      // Não quer comprar cartas
+      // Faz a pontuação
+      pontuacaoUsuario = calculaPontuacao(cartasUsuario)
+      // Fim da vez do Usuário
+   }
+
+   // Vez do Computador:
+   // Enquanto a pontuação for menor ou igual à do Usuário
+   pontuacaoComputador = calculaPontuacao(cartasComputador)
+   while((pontuacaoComputador <= pontuacaoUsuario) && (pontuacaoUsuario <= 21)) {
+      // Compra mais uma carta
+      novaCarta(cartasComputador)
+      // Faz a pontuação
+      pontuacaoComputador = calculaPontuacao(cartasComputador)
+   }
+
+   // Se a pontuação for maior do que 21
+   if(pontuacaoComputador > 21) {
+      alert(
+      `Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. Sua pontuação é ${pontuacaoUsuario}.` +
+      "\n" + 
+      `As cartas do computador são ${retornaCartasReveladas(cartasComputador)}. A pontuação do computador é ${pontuacaoComputador}.` +
+      "\n" +
+      "O usuário ganhou!")
+   } else {
+      // Fim da vez do Computador
+      // Imprime os resultados
+      if((pontuacaoComputador > pontuacaoUsuario) && (pontuacaoComputador <= 21)) {
+         alert(`Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. Sua pontuação é ${pontuacaoUsuario}.` +
+         "\n" + 
+         `As cartas do computador são ${retornaCartasReveladas(cartasComputador)}. A pontuação do computador é ${pontuacaoComputador}.` +
+         "\n" +
+         "O computador ganhou!")
+      } else if ((pontuacaoUsuario > pontuacaoComputador) && (pontuacaoUsuario <= 21)) {
+         alert(`Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. Sua pontuação é ${pontuacaoUsuario}.` +
+         "\n" + 
+         `As cartas do computador são ${retornaCartasReveladas(cartasComputador)}. A pontuação do computador é ${pontuacaoComputador}.` +
+         "\n" +
+         "O usuário ganhou!")
+      } else if (pontuacaoUsuario === pontuacaoComputador) {
+         alert(`Suas cartas são ${retornaCartasReveladas(cartasUsuario)}. Sua pontuação é ${pontuacaoUsuario}.` +
+         "\n" + 
+         `As cartas do computador são ${retornaCartasReveladas(cartasComputador)}. A pontuação do computador é ${pontuacaoComputador}.` +
+         "\n" +
+         "Empate!")
+      }
+   }
 } else {
    alert("O jogo acabou.")
 }
