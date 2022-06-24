30a63e1e7f7d61167c0ce19ebaf9a9e183a5ad94
Daniela Pinheiro <danie_klem@hotmail.com>
2022-06-24 15:57:46 -0300

    Renderização condicional de telas e requisição getAllPlaylists

--- modulo2/projeto-labefy/labefy/src/App.js
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -1,40 +1,75 @@
 import React from 'react'
 import TelaListaPlaylists from './pages/TelaListaPlaylists/TelaListaPlaylists'
 import TelaDetalhesPlaylists from './pages/TelaDetalhesPlaylists/TelaDetalhesPlaylists'
+import axios from 'axios'
+import { url_base } from './constants/url.js'
 
 class App extends React.Component {
   state = {
     telaAtual: "listaPlaylists",
-    // playlists: [],
+    playlists: [],
   }
 
-  // componentDidMount() {}
+  // Funções de ciclo de vida
+  componentDidMount() {
+    this.getAllPlaylists()
+  }
   // componentDidUpdate() {}
+
+  // Requisições
+  getAllPlaylists = () => {
+    axios.get(url_base, {
+      headers: {
+        Authorization: 'daniela-pinheiro-ailton'
+      }
+    }).then((response) => {
+      this.setState({playlists: response.data.result.list})
+    }).catch((error) => {
+      console.log(error.response)
+    })
+  }
 
   // Funções de renderização condicional de telas
   mudaTela = () => {
     switch (this.state.telaAtual) {
       case "listaPlaylists":
-        return <TelaListaPlaylists />
+        return <TelaListaPlaylists
+          listaPlaylists={this.state.playlists}
+          detalhesPlaylists={this.onClickMudaTelaDetalhes}
+        />
       case "detalhesPlaylists":
         return <TelaDetalhesPlaylists />
+      // case "editarPlaylists":
+      //   return </>
+      // case "criarPlaylists":
+      //   return </>
       default:
-        return <TelaListaPlaylists />
+        return <TelaListaPlaylists /> //Tela de Criar??
     }
   } 
-  onClickMudaTela = () => {
-    if(this.state.telaAtual === "listaPlaylists") {
-      this.setState({telaAtual: "detalhesPlaylists"})
-    } else {
-      this.setState({telaAtual: "listaPlaylists"})
-    }
-    
+
+  // Funções de mudança entre telas
+  onClickMudaTelaLista = () => {
+    this.setState({telaAtual: "listaPlaylists"})
+  }
+  onClickMudaTelaDetalhes = () => {
+    this.setState({telaAtual: "detalhesPlaylists"})
+  }
+  onClickMudaTelaEditar = () => {
+    this.setState({telaAtual: "editarPlaylists"})
+  }
+  onClickMudaTelaCriar = () => {
+    this.setState({telaAtual: "criarPlaylists"})
   }
 
   render() {
+
     return <div>
       {this.mudaTela()}
-      <button onClick={this.onClickMudaTela}>Mudar de Tela</button>
+      <button onClick={this.onClickMudaTelaLista}>Lista</button>
+      <button onClick={this.onClickMudaTelaDetalhes}>Detalhes</button>
+      <button onClick={this.onClickMudaTelaCriar}>Criar</button>
+      <button onClick={this.onClickMudaTelaEditar}>Editar</button>
     </div>
   }
 }
--- modulo2/projeto-labefy/labefy/src/pages/TelaListaPlaylists/TelaListaPlaylists.js
+++ modulo2/projeto-labefy/labefy/src/pages/TelaListaPlaylists/TelaListaPlaylists.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default class TelaListaPlaylists extends React.Component {
     render() {
-        return <div>Lista de Playlists</div>
+        const listaPlaylists = this.props.listaPlaylists.map((item) => {
+            return <li key={item.id} onClick={this.props.detalhesPlaylists}>{item.name}</li>
+        })
+
+        return <div>{listaPlaylists}</div>
     }
 }
