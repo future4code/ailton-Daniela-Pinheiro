6e99396e08c36740ac743007410de19957223c39
Daniela Pinheiro <danie_klem@hotmail.com>
2022-08-09 14:41:42 -0300

    Desafios 1 e 2

--- modulo4/node-package-json/exercicio-1/index.js
+++ modulo4/node-package-json/exercicio-1/index.js
@@ -8,5 +8,13 @@
 // console.log(`Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos.`)
 
 // c)
-const novaIdade = Number(process.argv[3]) + 7
-console.log('\x1b[31m', `Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos. Em sete anos você terá ${novaIdade} anos.`, '\x1b[0m')
+if(process.argv[2] && process.argv[3]) {
+    if(!isNaN(process.argv[3])) {
+        const novaIdade = Number(process.argv[3]) + 7
+        console.log('\x1b[36m', `Olá, ${process.argv[2]}! Você tem ${process.argv[3]} anos. Em sete anos você terá ${novaIdade} anos.`, '\x1b[0m')
+    } else {
+        console.log('\x1b[31m', "É necessário que o último parâmetro seja um número!", '\x1b[0m')
+    }
+} else {
+    console.log('\x1b[31m', "É necessário passar 2 parâmetros!", '\x1b[0m')
+}
--- modulo4/node-package-json/exercicio-2/index.js
+++ modulo4/node-package-json/exercicio-2/index.js
@@ -1,15 +1,23 @@
 // EXERCÍCIO 2
-switch(process.argv[2]) {
-    case "add" :
-        console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) + Number(process.argv[4]), '\x1b[0m')
-        break
-    case "sub" :
-        console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) - Number(process.argv[4]), '\x1b[0m')
-        break
-    case "mult" :
-        console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) * Number(process.argv[4]), '\x1b[0m')
-        break
-    case "div" :
-        console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) / Number(process.argv[4]), '\x1b[0m')
-        break
+if(process.argv[2] && process.argv[3] && process.argv[4]) {
+    if((process.argv[2].includes("add" || "sub" || "mult" || "div")) && !isNaN(process.argv[3]) && !isNaN(process.argv[4])) {
+        switch(process.argv[2]) {
+            case "add" :
+                console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) + Number(process.argv[4]), '\x1b[0m')
+                break
+            case "sub" :
+                console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) - Number(process.argv[4]), '\x1b[0m')
+                break
+            case "mult" :
+                console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) * Number(process.argv[4]), '\x1b[0m')
+                break
+            case "div" :
+                console.log('\x1b[36m', "Resultado: ", Number(process.argv[3]) / Number(process.argv[4]), '\x1b[0m')
+                break 
+        }
+    } else {
+        console.log('\x1b[31m', "É necessário que o primeiro parâmetro seja uma operação('add', 'sub', 'mult' ou 'div') e os dois últimos parâmetros sejam números!", '\x1b[0m')
+    }
+} else {
+    console.log('\x1b[31m', "É necessário passar 3 parâmetros!", '\x1b[0m')
 }
