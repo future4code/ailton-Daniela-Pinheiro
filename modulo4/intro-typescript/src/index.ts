6cb1f7fedad8fff39b3e09c946aa52e04e4c555a
Daniela Pinheiro <danie_klem@hotmail.com>
2022-08-10 16:58:39 -0300

    Desafios

--- modulo4/intro-typescript/src/index.ts
+++ modulo4/intro-typescript/src/index.ts
@@ -15,13 +15,11 @@ function checaTriangulo(a: number, b: number, c: number): string {
 
 
 // EXERCÍCIO 2 
-// ???????
-function imprimeTresCoresFavoritas(): void {
-    const cor1: string = prompt("Insira sua primeira cor favorita")
-    const cor2: string = prompt("Insira sua segunda cor favorita")
-    const cor3: string = prompt("Insira sua terceira cor favorita")
+function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string): void {
     console.log([cor1, cor2, cor3])
 }
+
+// imprimeTresCoresFavoritas('azul', 'vermelho', 'laranja')
 
 // EXERCÍCIO 3
 function checaAnoBissexto(ano: number): boolean {
@@ -75,4 +73,57 @@ function checaRenovacaoRG(anoAtual: number, anoNascimento: number, anoEmissao: n
     }
 }
 
-// console.log(checaRenovacaoRG(2022, 1993, 2011))   
+// console.log(checaRenovacaoRG(2022, 1993, 2011))
+
+
+// EXERCÍCIO 6
+function operaDoisNumeros (numero1: number, numero2: number): void {
+    const soma: number = numero1 + numero2
+    const subtração: number = numero1 - numero2
+    const multiplicação: number = numero1 * numero2
+    const maior: number = (numero1 > numero2) ? numero1 : numero2
+
+    console.log(`Soma: ${soma}\nSubtração: ${subtração}\nMultiplicação: ${multiplicação}\nMaior número: ${maior}`)
+}
+
+// operaDoisNumeros(2,3)
+
+
+// EXERCÍCIO 7
+function converteEmRNA (dna: string): string {
+    return dna.replace(/A/g, "U").replace(/T/g, "A").replace(/C/g, "g").replace(/G/g, "C").toUpperCase()
+}
+
+// console.log(converteEmRNA("ATTGCTGCGCATTAACGACGCGTA"))
+
+// EXERCÍCIO 8
+function inverteString (string: string): string {
+    const caracteres: string[] = string.split("")
+    const caracteresInvertidos: string[] = caracteres.reverse()
+    const stringInvertida: string = caracteresInvertidos.join("")
+
+    return stringInvertida
+}
+
+// console.log(inverteString("acbdefghij"))
+
+
+// EXERCÍCIO 9
+function validaEstudanteLabenu (
+    idade: number,
+    ensinoMedio: boolean,
+    horasDisponiveis: number,
+    curso: string
+    ):boolean {
+    if((curso === "integral") && (idade > 18) && (ensinoMedio) && (horasDisponiveis >= 40)) {
+        return true
+    } else if ((curso === "noturno") && (idade > 18) && (ensinoMedio) && (horasDisponiveis >= 20)) {
+        return true
+    } else {
+        return false
+    }   
+}
+
+// console.log(validaEstudanteLabenu(20, true, 50, "integral"))
+// console.log(validaEstudanteLabenu(16, true, 40, "integral"))
+// console.log(validaEstudanteLabenu(38, true, 20, "noturno"))
