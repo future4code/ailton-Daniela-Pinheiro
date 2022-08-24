fe1a107b29279af1d0af76547be90584faa7d1f7
Daniela Pinheiro <danie_klem@hotmail.com>
2022-08-24 20:34:28 -0300

    Exercicios 1 a 3

--- modulo4/knex-js/knex-js.md
+++ modulo4/knex-js/knex-js.md
@@ -1,30 +1,91 @@
 ### Exercício 1
-**a)**
+**a)** Quando usamos o `raw`, o que recebemos de resposta vai além das tabelas criadas no banco de dados, e inclui informações do próprio banco, dentro de um grande *array*. Por isso, retorno da função *getActorById* encontra-se dentro de um *array* que está contido nesse grande *array* de dados diversos.
 
 **b)**
+```
+const getActorByName = async (name: string): Promise<any> => {
+    const result = await connection.raw(`
+        SELECT * FROM Actor WHERE name = '${name}'
+    `)
+    return result[0][0]
+}
+```
 
 **c)**
+```
+const countActorsByGender = async (gender: string): Promise<any> => {
+    const result = await connection.raw(`
+        SELECT COUNT(*) as count FROM Actor WHERE gender = '${gender}'
+    `)
+    return result[0][0].count
+}
+```
 
 ### Exercício 2
 **a)**
+```
+const changeSalary = async (id: string, salary: number): Promise<any> => {
+    await connection('Actor')
+        .where({ id: id })
+        .update({ salary: salary })
+}
+```
+
 **b)**
+```
+const deleteActor = async (id: string): Promise<any> => {
+    await connection('Actor')
+        .where({ id: id })
+        .del()
+}
+```
+
 **c)**
-**d)**
+```
+const averageSalary = async (gender: string): Promise<any> => {
+    const result = await connection('Actor')
+        .where({ gender: gender })
+        .avg('salary as average')
+    return result[0].average
+}
+```
 
 ### Exercício 3
 **a)**
+```
+app.get("/actor/:id", async (req, res) => {
+    try {
+      const id = req.params.id
+      const actor = await getActorById(id)
+  
+      res.status(200).send(actor)
+    } catch (err: any) {
+      res.status(400).send({ message: err.message, })
+    }
+})
+```
+
 **b)**
-**c)**
-**d)**
+```
+app.get("/actor", async (req, res) => {
+    try {
+        const gender: string = req.query.gender as string
+        const actors = await countActorsByGender(gender)
+
+        res.status(200).send({ quantity: actors })
+    } catch (err: any) {
+        res.status(400).send({ message: err.message })    
+    }
+})
+```
 
 ### Exercício 4
 **a)**
-**b)**
-**c)**
-**d)**
+```
 
-### Exercício 5
-**a)**
+```
+
 **b)**
-**c)**
-**d)**
+```
+
+```
--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -23,8 +23,109 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// Endpoints
 
+// Exercício 1
+// a
+const getActorById = async (id: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM Actor WHERE id = '${id}'
+    `)
+  
+    return result[0][0]
+}
+
+app.get("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id
+        console.log(await getActorById(id))
+        res.end()
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).send("Unexpected error")
+    }
+})
+
+// b
+const getActorByName = async (name: string): Promise<any> => {
+    const result = await connection.raw(`
+        SELECT * FROM Actor WHERE name = '${name}'
+    `)
+    return result[0][0]
+}
+
+app.get("/users", async (req, res) => {
+    try {
+        const name: string = req.query.name as string
+        console.log(await getActorByName(name))
+        res.end()    
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).send("Unexpected error")
+    }
+})
+
+// c
+const countActorsByGender = async (gender: string): Promise<any> => {
+    const result = await connection.raw(`
+        SELECT COUNT(*) as count FROM Actor WHERE gender = '${gender}'
+    `)
+    return result[0][0].count
+}
+
+
+// Exercicio 2
+// a)
+const changeSalary = async (id: string, salary: number): Promise<any> => {
+    await connection('Actor')
+        .where({ id: id })
+        .update({ salary: salary })
+}
+
+// b)
+const deleteActor = async (id: string): Promise<any> => {
+    await connection('Actor')
+        .where({ id: id })
+        .del()
+}
+
+// c)
+const averageSalary = async (gender: string): Promise<any> => {
+    const result = await connection('Actor')
+        .where({ gender: gender })
+        .avg('salary as average')
+    return result[0].average
+}
+
+// Exercicio 3
+// a) 
+app.get("/actor/:id", async (req, res) => {
+    try {
+      const id = req.params.id
+      const actor = await getActorById(id)
+  
+      res.status(200).send(actor)
+    } catch (err: any) {
+      res.status(400).send({ message: err.message, })
+    }
+})
+
+// b)
+app.get("/actor", async (req, res) => {
+    try {
+        const gender: string = req.query.gender as string
+        const actors = await countActorsByGender(gender)
+
+        res.status(200).send({ quantity: actors })
+    } catch (err: any) {
+        res.status(400).send({ message: err.message })    
+    }
+})
+
+// Exercicio 4
+// a)
+
+
+// b)
 
 
 // Servidor
