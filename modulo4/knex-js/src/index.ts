047251a74f2843d79a630d287f8d392174ed493c
Daniela Pinheiro <danie_klem@hotmail.com>
2022-08-24 17:19:13 -0300

    Configurações iniciais

---
+++ modulo4/knex-js/src/index.ts
@@ -0,0 +1,38 @@
+import express from "express"
+import cors from "cors"
+import knex from "knex"
+import dotenv from "dotenv"
+import { AddressInfo } from "net"
+
+// Conexão com banco de dados
+dotenv.config()
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+})
+
+// Aplicação
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// Endpoints
+
+
+
+// Servidor
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo
+       console.log(`Server is running in http://localhost: ${address.port}`)
+    } else {
+       console.error(`Failure upon starting server.`)
+    }
+})
