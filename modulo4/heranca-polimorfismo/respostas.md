edfdbfbf1c64184dd4b730cce54a6909eb3d8e17
Daniela Pinheiro <danie_klem@hotmail.com>
2022-09-06 14:48:39 -0300

    Exercicios 2 a 5

--- modulo4/heranca-polimorfismo/respostas.md
+++ modulo4/heranca-polimorfismo/respostas.md
@@ -1,22 +1,37 @@
+## Herança
+
 ### Exercício 1
 **a)** Seria necessário criar um *getter* dentro da classe User que retornasse a senha, uma vez que essa é uma informação privada.
 
 **b)** Foi impressa uma vez, quando o novo usuário é criado.
 
 ### Exercício 2
+**a)** Foi impressa uma vez, quando o novo cliente é criado.
+
+**b)** Foi impressa uma vez, quando o novo cliente é criado, pois Customer é uma classe filha de User, e seu construtor referencia o construtor da classe mãe. Assim, quando se chama o construtor de Customer, o de User também é chamado.
+
+### Exercício 3
+**a)** Não seria possível imprimir o *password*, pois é uma informação privada da classe mãe e não há nenhum método que a retorne.
+
+
+## Polimorfismo
+
+### Exercício 1
 **a)**
 
 **b)** 
 
-### Exercício
+### Exercício 2
 **a)**
 
 **b)** 
-### Exercício
+
+### Exercício 3
 **a)**
 
 **b)** 
-### Exercício
+
+### Exercício 4
 **a)**
 
 **b)** 
--- modulo4/heranca-polimorfismo/src/index.ts
+++ modulo4/heranca-polimorfismo/src/index.ts
@@ -29,6 +29,12 @@ class User {
     public getName(): string {
         return this.name
     }
+
+    // Exercicio 4 e 5
+    public introduceYourself(): string {
+        // return "Olá, bom dia!"
+        return `Olá, sou ${this.name}. Bom dia!`
+    }
 }
 
 const newUser: User = new User("001", "aaaa@mail.com", "Alguém", "123456")
@@ -36,3 +42,38 @@ const newUser: User = new User("001", "aaaa@mail.com", "Alguém", "123456")
 console.log(`Nome: ${newUser.getName()}
 Email: ${newUser.getEmail()}
 Id: ${newUser.getId()}`)
+
+// Exercicio 2
+class Customer extends User {
+    public purchaseTotal: number = 0;
+    private creditCard: string;
+
+    constructor(
+        id: string,
+        email: string,
+        name: string,
+        password: string,
+        creditCard: string
+    ) {
+        super(id, email, name, password);
+        console.log("Chamando o construtor da classe Customer");
+        this.creditCard = creditCard;
+    }
+
+    public getCreditCard(): string {
+        return this.creditCard;
+    }
+}
+
+const newCustomer: Customer = new Customer("002", "aaabbb@mail.com", "Fulano", "1234567", "12345678901088")
+
+// Exercicio 3
+console.log(`Nome: ${newCustomer.getName()}
+Email: ${newCustomer.getEmail()}
+Id: ${newCustomer.getId()}
+Cartão: ${newCustomer.getCreditCard()}
+Compras: ${newCustomer.purchaseTotal}`)
+
+// Exercicio 4 e 5
+console.log(newCustomer.introduceYourself(),
+newUser.introduceYourself())
