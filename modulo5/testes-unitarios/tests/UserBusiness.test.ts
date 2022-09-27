2243cc95415ea6664acdf12a015f310c0f596505
Daniela Pinheiro <danie_klem@hotmail.com>
2022-09-27 16:05:07 -0300

    Exercicio 1

---
+++ modulo5/testes-unitarios/tests/UserBusiness.test.ts
@@ -0,0 +1,28 @@
+import {describe, expect, test} from '@jest/globals'
+import { UserBusiness } from "../src/business/UserBusiness"
+import { UserDatabaseMock } from "./mocks/UserDatabaseMock"
+import { IdGeneratorMock } from "./mocks/IdGeneratorMock"
+import { HashManagerMock } from "./mocks/HashManagerMock"
+import { AuthenticatorMock } from "./mocks/AuthenticatorMock"
+import { ISignupInputDTO } from '../src/models/User'
+
+describe("Testando a UserBusiness", () => {
+    const userBusiness = new UserBusiness(
+        new UserDatabaseMock(),
+        new IdGeneratorMock(),
+        new HashManagerMock(),
+        new AuthenticatorMock()
+    )
+
+    test("Testando o signup: retorna um token quando o cadastro é bem sucedido.", async() => {
+        const input: ISignupInputDTO = {
+            name: "Fulano",
+            email: "fulano@mail.com",
+            password: "bananinha"
+        }
+
+        const response = await userBusiness.signup(input)
+
+        expect(response.token).toBe("token-mock-normal")
+    })
+})
