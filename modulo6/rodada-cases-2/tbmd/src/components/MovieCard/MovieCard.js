ca1e2e68d4355acd7e0570475bb1a4d34a433241
Daniela Pinheiro <danie_klem@hotmail.com>
2022-10-13 19:52:13 -0300

    Renderização da lista de filmes, estilização de PopularMoviesPage e MovieCard

--- modulo6/rodada-cases-2/tbmd/src/components/MovieCard/MovieCard.js
+++ modulo6/rodada-cases-2/tbmd/src/components/MovieCard/MovieCard.js
@@ -1,17 +1,16 @@
 import React from "react"
 import "./MovieCard.css"
 import { IMG_BASE_URL } from "../../constants/urls"
-import { useRequestData } from "../../hooks/useRequestData"
 
-export default function MovieCard() {
-
-    const fc = useRequestData({}, '/movie/550')
+export default function MovieCard(props) {
+    const movie = props.movie
+    const releaseDate = new Date(movie.release_date).toLocaleDateString()
 
     return <div className="Movie-card">
-        <img className="Poster" src={IMG_BASE_URL + fc.poster_path} />
+        <img className="Poster" src={IMG_BASE_URL + movie.poster_path} />
         <div className="Title-card">
-            <h4 className="Movie-title">{fc.title}</h4>
-            <h4 className="Release-date">{fc.release_date}</h4>
+            <h4 className="Movie-title">{movie.title}</h4>
+            <h4 className="Release-date">{releaseDate}</h4>
         </div>
     </div>
 }
--- modulo6/rodada-cases-2/tbmd/src/pages/PopularMoviesPage/PopularMoviesPage.js
+++ modulo6/rodada-cases-2/tbmd/src/pages/PopularMoviesPage/PopularMoviesPage.js
@@ -1,18 +1,24 @@
 import React from "react"
+import "./PopularMoviesPage.css"
 import MovieCard from "../../components/MovieCard/MovieCard"
-import { IMG_BASE_URL } from "../../constants/urls"
 import { useRequestData } from "../../hooks/useRequestData"
 
 export default function PopularMoviesPage() {
-    // const movies = useRequestData([], '/movie/popular')
+    const movies = useRequestData([], '/movie/popular')
     // const fc = useRequestData({}, '/movie/550')
 
-    // const movie = movies.results && movies.results.map(movie => {
-    //     return <div key={movie.id}>{movie.title}</div>
-    // })
+    const movie = movies.results && movies.results.map(movie => {
+        return <div key={movie.id}>{movie.title}</div>
+    })
 
-    return <MovieCard />
-    // <div>
-
-    // </div>
+    return <div className="Movies-container">
+        <div className="Tags">
+            <h1>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
+        </div>
+        <div className="Popular-movies">
+            {movies.results && movies.results.map(movie => {
+            return <MovieCard movie={movie} />
+            })}
+        </div>
+    </div>
 }
