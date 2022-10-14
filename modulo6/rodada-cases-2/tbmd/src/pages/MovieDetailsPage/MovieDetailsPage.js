6a4ce224bd120876b1431b700541d87f79bd6604
Daniela Pinheiro <danie_klem@hotmail.com>
2022-10-14 10:26:23 -0300

    Exibição de informações do filme na MovieDetailsPage

--- modulo6/rodada-cases-2/tbmd/src/pages/MovieDetailsPage/MovieDetailsPage.js
+++ modulo6/rodada-cases-2/tbmd/src/pages/MovieDetailsPage/MovieDetailsPage.js
@@ -1,7 +1,18 @@
 import React from "react"
+import { useParams } from "react-router-dom"
+import { IMG_BASE_URL } from "../../constants/urls"
+import { useRequestData } from "../../hooks/useRequestData"
 
 export default function MovieDetailsPage() {
+    const params = useParams()
+
+    const movie = useRequestData({}, `/movie/${params.id}`)
+
     return <div>
-        Detalhes do filme
+        {movie && <div>
+            <h3>{movie.title}</h3>
+            <img src={IMG_BASE_URL + movie.poster_path} alt={movie.title} />
+            <p>{movie.overview}</p>
+        </div>}
     </div>
 }
