330eb3db4df5da0ea98c43d7ccf0362264bb4ffa
Daniela Pinheiro <danie_klem@hotmail.com>
2022-10-14 13:27:47 -0300

    Paginação inicial

--- modulo6/rodada-cases-2/tbmd/src/hooks/useRequestData.js
+++ modulo6/rodada-cases-2/tbmd/src/hooks/useRequestData.js
@@ -3,11 +3,11 @@ import axios from "axios"
 import { API_KEY } from "../constants/authorization"
 import { API_BASE_URL } from "../constants/urls"
 
-export const useRequestData = (initialData, pathParams) => {
+export const useRequestData = (initialData, pathParams, page) => {
     const [data, setData] = useState(initialData)
 
     useEffect(() => {
-        axios.get(`${API_BASE_URL}${pathParams}${API_KEY}&language=pt-BR`, {
+        axios.get(`${API_BASE_URL}${pathParams}${API_KEY}&language=pt-BR${page}`, {
         }).then(response => {
             setData(response.data)
         }).catch(error => {
--- modulo6/rodada-cases-2/tbmd/src/pages/PopularMoviesPage/PopularMoviesPage.js
+++ modulo6/rodada-cases-2/tbmd/src/pages/PopularMoviesPage/PopularMoviesPage.js
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import "./PopularMoviesPage.css"
 import MovieCard from "../../components/MovieCard/MovieCard"
 import { useRequestData } from "../../hooks/useRequestData"
 
 export default function PopularMoviesPage() {
-    const movies = useRequestData([], '/movie/popular')
+    const [page, setPage] = useState(1)
+
+    const onClickPage = (number) => {
+        setPage(number)
+    }
+
+    const movies = useRequestData([], '/movie/popular', `&page=${page}`)
 
     return <div className="Movies-container">
         <div className="Tags">
@@ -14,6 +20,12 @@ export default function PopularMoviesPage() {
             {movies.results && movies.results.map(movie => {
             return <MovieCard movie={movie} />
             })}
+        </div>
+        <div className="Pagination">
+            <p onClick={() => onClickPage(1)}><strong>1</strong></p>
+            <p onClick={() => onClickPage(2)}><strong>2</strong></p>
+            <p onClick={() => onClickPage(3)}><strong>3</strong></p>
+            <p onClick={() => onClickPage(4)}><strong>4</strong></p>
         </div>
     </div>
 }
