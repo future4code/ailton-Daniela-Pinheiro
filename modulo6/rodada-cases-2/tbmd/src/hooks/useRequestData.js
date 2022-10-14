4dcb3ab0cbf0d6088fc17e2ec1a359bfdefd374b
Daniela Pinheiro <danie_klem@hotmail.com>
2022-10-14 17:20:15 -0300

    Estilização MovieDetailsPage, requisição de datas de lançamento

--- modulo6/rodada-cases-2/tbmd/src/hooks/useRequestData.js
+++ modulo6/rodada-cases-2/tbmd/src/hooks/useRequestData.js
@@ -5,9 +5,10 @@ import { API_BASE_URL } from "../constants/urls"
 
 export const useRequestData = (initialData, pathParams, page) => {
     const [data, setData] = useState(initialData)
+    const pagination = page? page : ""
 
     useEffect(() => {
-        axios.get(`${API_BASE_URL}${pathParams}${API_KEY}&language=pt-BR${page}`, {
+        axios.get(`${API_BASE_URL}${pathParams}${API_KEY}&language=pt-BR${pagination}`, {
         }).then(response => {
             setData(response.data)
         }).catch(error => {
--- modulo6/rodada-cases-2/tbmd/src/pages/MovieDetailsPage/MovieDetailsPage.js
+++ modulo6/rodada-cases-2/tbmd/src/pages/MovieDetailsPage/MovieDetailsPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./MovieDetailsPage.css"
 import { useParams } from "react-router-dom"
 import { IMG_BASE_URL } from "../../constants/urls"
 import { useRequestData } from "../../hooks/useRequestData"
@@ -8,11 +9,46 @@ export default function MovieDetailsPage() {
 
     const movie = useRequestData({}, `/movie/${params.id}`)
 
-    return <div>
-        {movie && <div>
-            <h3>{movie.title}</h3>
-            <img src={IMG_BASE_URL + movie.poster_path} alt={movie.title} />
-            <p>{movie.overview}</p>
-        </div>}
-    </div>
+    const releaseDates = useRequestData([], `/movie/${params.id}/release_dates`)
+    const releaseDate = releaseDates && releaseDates.results && releaseDates.results.find(item => {
+        return item.iso_3166_1 === "BR"
+    })
+
+    const releaseDateBR = releaseDate && new Date(releaseDate.release_dates[0].release_date).toLocaleDateString()
+
+    const releaseYear = movie && new Date(movie.release_date).getFullYear()
+
+    const genres = movie && movie.genres && movie.genres.map(genre => {
+        return genre.name
+    }).join(", ")
+
+    return movie && <div className="Details-container">
+            <div className="Details-background" />
+
+            <img className="Details-poster" src={IMG_BASE_URL + movie.poster_path} alt={movie.title} />
+            
+            <div className="Movie-details">
+                <h2 id="Movie-title">{movie.title} ({releaseYear})</h2>
+                <div className="Technical-info">
+                    <p>{movie.vote_average}</p> {/* porcentagem (popularity???) */}
+                    <p>{releaseDateBR} (BR)</p>
+                    <p>{genres}</p>
+                    <p>{movie.runtime} min</p>
+                </div>
+                <h3>Sinopse</h3>
+                <p id="Details-overview">{movie.overview}</p>
+            </div>
+
+            <div className="Movie-cast">
+                <p>Elenco</p>
+            </div>
+
+            <div className="Movie-trailer">
+                <p>Trailer</p>
+            </div>
+
+            <div className="Recomendations">
+                <p>Recomendações</p>
+            </div>
+        </div>
 }
